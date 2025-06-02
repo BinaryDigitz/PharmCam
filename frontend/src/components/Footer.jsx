991799c5795fdde32ba70f2df354f4e0d2e3e3a8
Binary Digitz <borisndoh@gmail.com>
2025-06-02 16:02:29 +0100

    Add site map and robot.txt file

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { banginic_logo, verified } from '../assets/assets'
+import React from "react";
+import { banginic_logo, verified } from "../assets/assets";
 
 function Footer() {
   return (
-    <section className='text-center text-xs text-green-950/70 flex justify-center gap-4 my-2'>
+    <section className="text-center text-xs text-green-950/70 flex justify-center gap-4 my-2">
       <p>Developed & Powered by</p>
-       <p 
-       title='Visit developer'
-       className='text-black cursor-pointer flex gap-1 items-center'>
-        <img src={banginic_logo} width={17} alt={verified} loading='lazy' />
+      <a
+        href="http://banginic-1.onrender.com"
+        target="blank"
+        title="Visit developer"
+        className="text-black cursor-pointer flex gap-1 items-center"
+      >
+        <img src={banginic_logo} width={17} alt={verified} loading="lazy" />
         <span>Banginic</span>
-       </p>
+      </a>
     </section>
-  )
+  );
 }
 
-export default Footer
-
+export default Footer;
